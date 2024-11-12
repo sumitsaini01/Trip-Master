e032443f4a3185fc9fdaeeddeeb37390d3cb75bf
= <sumitsaini8710@gmail.com>
2024-11-13 00:46:42 +0530

    added a new page

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,4 +1,4 @@
-function JavaBlink() {
+/*function JavaBlink() {
     var blinks = document.getElementsByTagName('JavaBlink');
     for (var i = blinks.length - 1; i >= 0; i--) {
        var s = blinks[i];
@@ -10,3 +10,26 @@ function JavaBlink() {
  else if (window.addEventListener) window.addEventListener("load", JavaBlink, false);
  else if (window.attachEvent) window.attachEvent("onload", JavaBlink);
  else window.onload = JavaBlink;
+ */
+// script.js
+
+// Update the slider value display
+const mySlider = document.getElementById("mySlider");
+const result = document.getElementById("result");
+
+if (mySlider && result) {
+    mySlider.oninput = function() {
+        result.innerText = this.value;
+    };
+}
+
+// Blink effect for the Book Now button
+function JavaBlink() {
+    const blinks = document.getElementsByTagName('JavaBlink');
+    for (let i = 0; i < blinks.length; i++) {
+        blinks[i].style.visibility = (blinks[i].style.visibility === 'visible') ? 'hidden' : 'visible';
+    }
+    setTimeout(JavaBlink, 500);
+}
+
+document.addEventListener("DOMContentLoaded", JavaBlink);
